0c76f74dde42b18b1dd63be06096d5a2323b7fdd
James Jung <therealjamesjung@gmail.com>
2024-03-14 18:49:00 +0900

    remove header text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,30 @@
 <h2>
-    잭영의 투두리스트~
-    <a href="/">목록</a>
-    <a href="/new">추가</a>
+	<a href="/">목록</a>
+	<a href="/new">추가</a>
 </h2>
 
-<slot/>
+<slot />
 
 <style>
-    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
-    :global(:root) {
-        font-family: -apple-system, BlinkMacSystemFont, "Apple SD Gothic Neo", "Pretendard Variable", Pretendard, Roboto, "Noto Sans KR", "Segoe UI", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
-        color: white;
-        background-color: #1E1E1E;
-    }
+	@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css');
+	:global(:root) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo', 'Pretendard Variable',
+			Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', 'Apple Color Emoji',
+			'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
+		color: white;
+		background-color: #1e1e1e;
+	}
 
-    :global(a, a:visited, a:hover, a:active) {
-        color: inherit;
-    }
+	:global(a, a:visited, a:hover, a:active) {
+		color: inherit;
+	}
 
-    :global(input) {
-        background-color: #292929;
-        color: white;
-        border: 2px solid #363636;
-        border-radius: 4px;
-        padding: 10px 16px;
-        outline: none;
-    }
+	:global(input) {
+		background-color: #292929;
+		color: white;
+		border: 2px solid #363636;
+		border-radius: 4px;
+		padding: 10px 16px;
+		outline: none;
+	}
 </style>
