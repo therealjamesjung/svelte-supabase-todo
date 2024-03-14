46a5f277e66c77cfd556a1322fbaecb0a4ac1d49
James Jung <therealjamesjung@gmail.com>
2024-03-14 17:52:33 +0900

    fix global css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,16 @@
 </h2>
 
 <slot/>
+
+<style>
+    @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
+    :global(:root) {
+        font-family: -apple-system, BlinkMacSystemFont, "Apple SD Gothic Neo", "Pretendard Variable", Pretendard, Roboto, "Noto Sans KR", "Segoe UI", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+        color: white;
+        background-color: #1E1E1E;
+    }
+
+    a, a:visited, a:hover, a:active {
+        color: inherit;
+    }
+</style>
